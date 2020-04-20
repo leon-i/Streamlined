ff7842c012d3d89f9a8c7e19d84605b3e5a8b825
IvanGit911 <wang.han.ivan@hotmail.com>
2020-04-20 11:40:28 -0400

    add show schema

---
+++ models/Show.js
@@ -0,0 +1,35 @@
+const mongoose = requier("mongoose");
+const Schema = mongoose.Schema;
+
+const ShowSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    required: true,
+  },
+  releaseDate: {
+    type: Date,
+    required: true,
+  },
+  Genres: {
+    type: Array,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+  provider: {
+    type: Schema.Types.ObjectId,
+    ref: "providers",
+  },
+});
+
+module.exports = Show = mongoose.model("show", ShowSchema);
