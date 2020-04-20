3180181d88084dbea1a104983e1c23391c76c7e1
leon-i <isakleon97@gmail.com>
2020-04-20 11:19:44 -0400

    User Auth Skeleton complete

---
+++ frontend/src/util/session_api_util.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+export const setAuthToken = token => {
+    if (token) {
+        return axios.defaults.headers.common['Authorization'] = token;
+    } else {
+        return axios.defaults.headers.common['Authorization'];
+    }
+}
+
+export const signup = (userData) => {
+    return axios.post('/api/users/register', userData);
+  };
+  
+export const login = (userData) => {
+    return axios.post('/api/users/login', userData);
+};
