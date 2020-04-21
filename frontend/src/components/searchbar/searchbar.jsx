3d385dfcd674e4cce9137c983244ed189490058f
leon-i <isakleon97@gmail.com>
2020-04-21 10:38:28 -0400

    Removed commented out code

--- frontend/src/components/searchbar/searchbar.jsx
+++ frontend/src/components/searchbar/searchbar.jsx
@@ -54,50 +54,6 @@ class SearchBar extends React.Component {
         })
     }
 
-    // requestMedia() {
-    //     const { mediaType, title } = this.state;
-    //     axios({
-    //         "method":"GET",
-    //         "url":"https://ivaee-internet-video-archive-entertainment-v1.p.rapidapi.com/entertainment/match/",
-    //         "headers":{
-    //             "content-type":"application/json",
-    //             "x-rapidapi-host":"ivaee-internet-video-archive-entertainment-v1.p.rapidapi.com",
-    //             "x-rapidapi-key": "ff0bd0bd75msh1ff9e2cc41c3334p19d376jsn54e9c50d9a13"
-    //         },"params":{
-    //             "Title": title,
-    //             "ProgramType": mediaType
-    //         }
-    //     })
-    //     .then((response)=> {
-    //         const media = response.data.ProgramMatches[response.data.ProgramMatches.length - 1];
-    //         this.setState({ media: media });
-    //         axios({
-    //             "method":"GET",
-    //             "url":"https://ivaee-internet-video-archive-entertainment-v1.p.rapidapi.com/entertainment/search/",
-    //             "headers":{
-    //                 "content-type":"application/json",
-    //                 "x-rapidapi-host":"ivaee-internet-video-archive-entertainment-v1.p.rapidapi.com",
-    //                 "x-rapidapi-key": "ff0bd0bd75msh1ff9e2cc41c3334p19d376jsn54e9c50d9a13"
-    //             },"params":{
-    //                 "Ids": this.state.media.Id,
-    //                 "Providers": 'Netflix'
-    //             }
-    //         })
-    //         .then ((response2) => {
-    //             if (response2.data.Hits.length) {
-    //                 this.setState({ providers: 'Netflix' });
-    //             }
-    //             debugger
-    //         })
-    //         .catch((error)=>{
-    //             console.log(error)
-    //         })
-    //     })
-    //     .catch((error)=>{
-    //         console.log(error)
-    //     })
-    // }
-
     handleChange(field) {
         return (e) => {
             this.setState({ [field]: e.target.value })
