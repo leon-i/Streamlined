e6984254c98314f1939a1ebb2e05553cc5589091
leon-i <isakleon97@gmail.com>
2020-05-22 17:23:01 -0400

    Added providers and add to queue button to show page

--- frontend/src/components/shows/show_detail.jsx
+++ frontend/src/components/shows/show_detail.jsx
@@ -1,15 +1,26 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+import QueueButton from '../search_results/queue_button';
 import "../../stylesheets/show-page.css"
 
 class ShowDetail extends React.Component {
   componentDidMount() {
-    this.props.requestShow(this.props.match.params.title);
+    const { match, requestShow, requestSearchResult, clearSearchResult } = this.props;
+    clearSearchResult();
+    requestShow(match.params.title);
+    requestSearchResult({ mediaType: 'Show', title: match.params.title });
   }
 
   render() {
-    const { show } = this.props;
+    const { show, searchResults } = this.props;
+    const searchResult = Object.values(searchResults)[0];
 
     if (!show) return null;
+    if (!searchResult) return (
+      <FontAwesomeIcon className='loading-icon' icon={faSpinner} spin />
+    );
+
     const {
       name,
       networks,
@@ -21,9 +32,17 @@ class ShowDetail extends React.Component {
       seasons,
     } = show;
 
-    return (
-      <div className="show-page">
+    const providersRender = searchResult.providers.map((provider, idx) => (
+      <li key={idx}>
+          <a href={`https://${provider}.com`}>
+              <img className='provider-logo' src={process.env.PUBLIC_URL + `/${provider}.png`} alt='stream-logo' /> 
+          </a>
+      </li>
+    ));
 
+    return (
+      <>
+      <div className="show-page">
         <img className="backdrop" src={`https://image.tmdb.org/t/p/w500${backdrop_path}`} />
 
         <div className="information">
@@ -38,11 +57,16 @@ class ShowDetail extends React.Component {
 
             <div>Rating: {vote_average}</div>
             <div>Votings: {vote_count}</div>
+            <ul>
+              { providersRender }
+            </ul>
           </div>
-
         </div>
-          
+          <div className='show-queue-btn-container'>
+            <QueueButton />
+          </div>
       </div>
+      </>
     );
   }
 }
--- frontend/src/components/shows/show_detail_container.js
+++ frontend/src/components/shows/show_detail_container.js
@@ -1,15 +1,20 @@
 import { connect } from "react-redux";
 import { requestShow } from "../../actions/show_actions";
+import { requestSearchResult, clearSearchResult, clearSearchErrors } from '../../actions/search_actions';
 import ShowDetail from "./show_detail";
 
 const msp = (state, ownProps) => {
     return {
       show: state.entities.shows.detail,
+      searchResults: state.entities.searchResults
     //   title: ownProps.match.params,
     };
 };
 const mdp = (dispatch) => ({
   requestShow: (title) => dispatch(requestShow(title)),
+  requestSearchResult: query => dispatch(requestSearchResult(query)),
+  clearSearchResult: () => dispatch(clearSearchResult()),
+  clearSearchErrors: () => dispatch(clearSearchErrors())
 });
 
 export default connect(msp, mdp)(ShowDetail);
